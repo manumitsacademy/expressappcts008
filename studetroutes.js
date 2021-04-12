ea21074ed996efd4cf0bcaadb7adaed86ead8adc
manumitsacademy <manumits.info@gmail.com>
2021-04-12 12:42:11 +0530

    'mongoclient'

--- studetroutes.js
+++ studetroutes.js
@@ -1,6 +1,40 @@
 var express = require("express");
+const { MongoClient } = require("mongodb");
 var router = express.Router();
 var students = require("./studentsmock")
+var mongoClient = require("mongodb").MongoClient;
+
+router.get("/getAllStudents",(req,res)=>{
+    MongoClient.connect("mongodb://localhost:27017",
+    { useUnifiedTopology: true },function(err,client){
+        var db = client.db('cts')
+        if(err){
+            console.log(err)
+        }
+        else{
+            db.collection('cts').find().toArray().then((data)=>{
+                res.json(data)
+            })
+        }        
+    })
+})
+router.get("/addStudent",(req,res)=>{
+    res.sendFile(__dirname+"/addStudent.html")
+})
+router.post("/addStudent",(req,res)=>{
+    MongoClient.connect("mongodb://localhost:27017",
+    { useUnifiedTopology: true },function(err,client){
+        var db = client.db('cts')
+        if(err){
+            console.log(err)
+        }
+        else{
+            db.collection('students').insertOne(req.body).then((data)=>{
+                res.json(data)
+            })
+        }        
+    })
+})
 router.get("/:pagenumber",(req,res)=>{
     var pn = parseInt(req.params.pagenumber);
     console.log(pn);
